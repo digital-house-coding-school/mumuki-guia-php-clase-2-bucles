01a772546a1104102a66c365941b637f19f294b8
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-21 20:01:56 +0000

    Mumuki Export on 2019-05-21 20:01:56 +0000

--- 00010_Los nombres de los cantantes/test.php
+++ 00010_Los nombres de los cantantes/test.php
@@ -1,70 +1,38 @@
-public function testDescriptionExample(): void {
-  $sol = '/*...content...*/';
-  
-  $cantantes = [
-    0 => [
-      "nombre" => "Luis",
-      "apellido" => "Miguel"
-    ],
-    1 => [
-      "nombre" => "Charly",
-      "apellido" => "Garcia"
-    ],
-    2 => [
-      "nombre" => "Joaquín",
-      "apellido" => "Sabina"
-    ]
-  ];
-  
-  $nombres = $this->bucles($cantantes);
-  
-  $cantidadOK = count($nombres) == 3;
-  $this->assertTrue($cantidadOK, "La cantidad de nombres resultados es incorrecta");
-  
-  $i = 0;
-  $nombresOK = true;
-  
-  foreach ($nombres as $nombre) {
-    $nombresOK = $nombresOK && $nombre == $cantantes[$i]["nombre"];
-    $i++;
-  }
-  
-  $this->assertTrue($nombresOK, "Si bien la cantidad de nombres es correcta, los nombres en sí no están bien");
-  
-  // Otra más
-  
-  $cantantes = [
-    0 => [
-      "nombre" => "Carlos",
-      "apellido" => "Miguel"
-    ],
-    1 => [
-      "nombre" => "Juan",
-      "apellido" => "Garcia"
-    ],
-    2 => [
-      "nombre" => "Enzo",
-      "apellido" => "Sabina"
-    ]
-  ];
-  
-  $nombres = $this->bucles($cantantes);
-  
-  $cantidadOK = count($nombres) == 3;
-  $this->assertTrue($cantidadOK, "La cantidad de nombres resultados es incorrecta");
-  
-  $i = 0;
-  $nombresOK = true;
-  
-  foreach ($nombres as $nombre) {
-    $nombresOK = $nombresOK && $nombre == $cantantes[$i]["nombre"];
-    $i++;
-  }
-  
-  $this->assertTrue($nombresOK, "Si bien la cantidad de nombres es correcta, los nombres en sí no están bien");
-  
-  $this->assertTrue(substr_count($sol, "foreach") > 0, "No utilizaste un foreach");
-  $this->assertTrue((substr_count($sol, "for ") + substr_count($sol, "for(")) == 0, "No deberías haber utilizado un for");
-}
+public function testDescriptionResultado(): void {
+    $solucao = '/*...content...*/';
 
-private /*...content...*/
+    $cantores = [
+      0 => [
+        "nome" => "Luiz",
+        "sobrenome" => "Gonzaga"
+      ],
+      1 => [
+        "nome" => "Tim",
+        "sobrenome" => "Maia"
+      ],
+      2 => [
+        "nome" => "Gilberto",
+        "sobrenome" => "Gil"
+      ]
+    ];
+
+    $nomes = $this->preencheNome($cantores);
+
+    $quantidade = count($nomes);
+    $this->assertTrue($quantidade == 3, "A quantidade de nomes no array está incorreta...");
+
+    $funcionando = true;
+    for ($i=0; $i < count($nomes); $i++) {
+      if ($nomes[$i] != $cantores[$i]["nome"]) {
+        $funcionando = false;
+        break;
+      }
+    }
+
+    $this->assertTrue($funcionando, "A quantidade de nomes está correta, porém os nomes estão errados :(");
+
+    $this->assertTrue(substr_count($solucao, "foreach") > 0, "Você precisa utilizar um foreach!");
+    $this->assertTrue(substr_count($solucao, "for ") == 0  || substr_count($solucao, "for(") == 0, "Parece que você tentou utilizar um for :(, tente utilizar um foreach :)");
+  }
+
+  private /*...content...*/
